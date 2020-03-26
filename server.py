f7b65860f85148b3db745f24aaf98eaf6979aec1
Garrett Watson <garrettbauer@gmail.com>
2020-03-26 11:16:35 +0900

    Convert predictor to a simple flask server

--- server.py
+++ server.py
@@ -1,19 +1,27 @@
+# AI Stuff
 import tensorflow as tf
 from transformers import *
 
-# This file includes code which was modified from https://github.com/huggingface/transformers/blob/master/examples/run_generation.py
+# Flask stuff
+from flask import Flask, url_for, make_response
+from flask_classful import FlaskView, route
+from flask_cors import CORS, cross_origin
+
+import json
+
+app = Flask(__name__)
+CORS(app)
+
 tokenizer = AutoTokenizer.from_pretrained("gpt2", eos_token=".")
 model = AutoModelWithLMHead.from_pretrained("./models", cache_dir=None, from_tf=False, state_dict=None)
 
-sequence = f"Donald Trump recently"
-
-tf.random.set_random_seed(0)
+sequence = f"A good life"
 
 input = tokenizer.encode(sequence, return_tensors="pt")
 sample_outputs = model.generate(
   input,
   do_sample=True,
-  max_length=150,
+  max_length=100,
   top_k=40,
   top_p=0.9,
   temperature=0.7,
